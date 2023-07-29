7bb5c17fee34e2dc9802c42347caa63bff1541e1
leon <leonsatoshi39.gmail.com>
2023-07-30 02:41:15 +0900

    emoji should be smaller

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "Hack:size=15", "NotoColorEmoji:size=15:antialias=true:autohint=true" };
+static char *fonts[]          = { "Hack:size=15", "NotoColorEmoji:size=14:antialias=true:autohint=true" };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
