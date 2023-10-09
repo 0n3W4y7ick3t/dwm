ecc8b81e716e8028ed5477445cf357787dec5257
satoshi <leonsatoshi39@gmail.com>
2023-10-09 12:06:29 +0900

    drop fonts relies on pixelsize

--- config.h
+++ config.h
@@ -21,9 +21,8 @@ static int smartgaps =
     0;                  /* 1 means no outer gap when there is only one window */
 static int showbar = 1; /* 0 means no bar */
 static int topbar = 1;  /* 0 means bottom bar */
-static char *fonts[] = {
-    "Hack:pixelsize=35",
-    "NotoColorEmoji:pixelsize=30:antialias=true:autohint=true"};
+static char *fonts[] = {"Hack Nerd Font:size=12",
+                        "NotoColorEmoji:size=12:antialias=true:autohint=true"};
 static char normbgcolor[] = "#222222";
 static char normbordercolor[] = "#444444";
 static char normfgcolor[] = "#bbbbbb";
@@ -350,4 +349,3 @@ static Button buttons[] = {
     {ClkTagBar, 0, Button5, shiftview, {.i = 1}},
     {ClkRootWin, 0, Button2, togglebar, {0}},
 };
-
